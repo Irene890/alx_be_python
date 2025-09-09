ba98f7ac912da4a825cb08cc5c2869cad34199d9
Irene890 <munirene20@gmail.com>
2025-09-09 20:04:13 +0300

    2050 future age

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,13 @@
+# Sript that asks the user for their current age and then calculates how old they will be in 2050
+futureyear = 2050
+currentyear = 2023
+
+currentage= int(input('How old are you? '))
+
+additionyears = futureyear - currentyear 
+
+futureage = currentage + additionyears
+
+print(f"In 2050, you will be {futureage} years old.")
+
+                         
