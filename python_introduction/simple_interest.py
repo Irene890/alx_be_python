05555db5f8df7848cca2da55fbd9f603bd80e7ef
Irene890 <munirene20@gmail.com>
2025-09-09 19:12:45 +0300

    Simple interest file

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+ # Calculating the simple interest using the I = P * R * T storing the result in a variable named interest
+
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+print(f"The simple interest is : {interest}")
