8a3fdc3f8d9247f6f6d615d7e3b9b34904fe6679
Irene890 <munirene20@gmail.com>
2025-09-18 20:56:17 +0300

    daily reminder

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -11,11 +11,17 @@ match priority:
     case "high":
         if time_bound == "yes":
             print(f"{task} is a {priority} priority task that requires immediate attention today!")
+        else:
+            print(f"{task} is a {priority} priority task. Consider completing it when you have free time.")
     case "medium":
         if time_bound == "yes":
-            print(f"{task} is a {priority} priority task. Consider planning for it.")
+            print(f"{task} is a {priority} priority task that requires immediate attention today!")
+        else:
+            print(f"{task} is a {priority} priority task. Consider completing it when you have free time.")
     case "low":
-        if time_bound == "no":
+        if time_bound == "yes":
+            print(f"{task} is a {priority} priority task that requires immediate attention today!")
+        else:
             print(f"{task} is a {priority} priority task. Consider completing it when you have free time.")
     case _:
         print("")
