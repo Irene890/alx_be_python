569f71d09eefd1b3a28f0c81ddf9d9d3e649658f
Irene890 <munirene20@gmail.com>
2025-09-26 16:59:41 +0300

    Arithmetic operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,12 +3,15 @@
 # The operation parameter accepts four possible values: 'add', 'subtract', 'multiply', or 'divide'.
 # The function should execute the arithmetic operation based on the operation parameter and the numerical values provided.
 # For division, include handling for division by zero, returning a specific message or value that your main.py script can recognize and display appropriately.
+num1 = float()
+num2 = float()
+operation = ""
 
 def perform_operation(num1,num2,operation):
-    # num1 = float(input("Enter num1: "))
-    # num2 = float(input("Enter num2: "))
-    # operation = input("Enter operation 'add', 'subtract', 'multiply', or 'divide': ").lower()
-    
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (add, subtract, multiply, divide): ").lower()
+
     match operation:
         case "add":
             result = num1 + num2
@@ -27,3 +30,4 @@ def perform_operation(num1,num2,operation):
                 print(f"Result: {result}")
         case _:
             print("I don't recognize that.")
+
