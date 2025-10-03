db1268ec758b6379d6c0dca9b879102ebc89c3af
Irene890 <munirene20@gmail.com>
2025-10-03 18:27:33 +0300

    Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -11,17 +11,11 @@ class BankAccount:
             return f"ERROR: Deposit must be positive."
 
     def withdraw(self, amount):     
-        if amount <= 0:
-            return "Amount should higher that current balance."
-            
-        if self._account_balance >= amount:
+        if 0 < amount:
             self._account_balance -= amount
-            return f"Withdrew:${amount:.0f}"
-            
-        else:
-            return f"Insufficient funds."
-            
-        
+            return True
+        return False
+
     def display_balance(self):
         print( f"Current Balance: ${self._account_balance}")
 
