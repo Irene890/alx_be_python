f45ef494ad4ce05c1030b81796bcfc4977504885
Irene890 <munirene20@gmail.com>
2025-10-04 20:17:03 +0300

    division calculator that robustly handles errors

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -11,7 +11,7 @@ def safe_divide(numerator, denominator):
         den = float(denominator)
         num1 = num / den
     except ZeroDivisionError:
-        return("Error: Cannot divide by Zero.")
+        return("Error: Cannot divide by zero.")
     except ValueError:
         return("Error: Please enter numeric values only.")
     else:
